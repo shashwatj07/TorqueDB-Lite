aeaeb88e88f6352e76bd4951d0eaf868ee89ce4a
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2023-01-19 15:15:22 +0530

    EdgeServer refactored

--- src/main/java/com/dreamlab/server/EdgeServerLocal.java
+++ src/main/java/com/dreamlab/server/EdgeServerLocal.java
@@ -5,21 +5,20 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
-import java.net.Inet4Address;
 import java.util.UUID;
 
-public class EdgeServer {
+public class EdgeServerLocal {
 
-    private EdgeServer() {
+    private EdgeServerLocal() {
     }
 
     public static void main(String... args) throws IOException, InterruptedException {
         final UUID edgeId = UUID.randomUUID();
-        final String edgeIp = Inet4Address.getLocalHost().getHostAddress();
-        final int edgePort = Integer.parseInt(args[0]);
-        final int heartbeatTtlSecs = Integer.parseInt(args[1]);
-        final String fogsConfigFilePath = args[2];
-        final String trajectoryFilePath = args[3];
+        final String edgeIp = args[0];
+        final int edgePort = Integer.parseInt(args[1]);
+        final int heartbeatTtlSecs = Integer.parseInt(args[2]);
+        final String fogsConfigFilePath = args[3];
+        final String trajectoryFilePath = args[4];
         Server server = ServerBuilder
                 .forPort(edgePort)
                 .addService(new EdgeService(edgeId, edgeIp, edgePort, heartbeatTtlSecs, fogsConfigFilePath, trajectoryFilePath))
