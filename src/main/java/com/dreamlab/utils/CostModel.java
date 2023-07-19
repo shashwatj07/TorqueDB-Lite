ab70a0b7d2f27819328c3cebd4e8b1c8bfb570e8
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2023-07-19 13:37:04 -0700

    Modify QP2 and QP3 to choose local fog if that suffices

--- src/main/java/com/dreamlab/utils/CostModel.java
+++ src/main/java/com/dreamlab/utils/CostModel.java
@@ -50,78 +50,105 @@ public final class CostModel {
         }
     }
 
-    public static List<ExecPlan> QP2(HashSet<BlockIdReplicaMetadata> blockIdReplicaMetadataSet, Map<UUID, FogPartition> fogPartitions, UUID fogId) {
-        // minimize the number of blocks per fog
-
-        Map<UUID, UUID> mapping = new HashMap<>();
-        Map<UUID, Set<UUID>> fogs = new HashMap<>();
-        Map<UUID, Set<UUID>> blocks = new HashMap<>();
+    public static boolean canQueryLocalFog(HashSet<BlockIdReplicaMetadata> blockIdReplicaMetadataSet, Map<UUID, FogPartition> fogPartitions, UUID fogId) {
+        boolean canQueryLocalFog = true;
+        // Assume current fog is active (obviously)
+        if (blockIdReplicaMetadataSet.size() > 7) {
+            canQueryLocalFog = false;
+        }
         for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
-            UUID blockId = Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId());
-            blocks.put(blockId, new HashSet<>());
-            for (BlockReplica blockReplica : blockIdReplicaMetadata.getReplicasList()) {
-                UUID replicaFogId = Utils.getUuidFromMessage(blockReplica.getDeviceId());
-                blocks.get(blockId).add(replicaFogId);
-                if (!fogs.containsKey(replicaFogId)) {
-                    fogs.put(replicaFogId, new HashSet<>());
-                }
-                fogs.get(replicaFogId).add(blockId);
+            if (blockIdReplicaMetadata.getReplicasList().stream().noneMatch(replica -> Utils.getUuidFromMessage(replica.getDeviceId()).equals(fogId))) {
+                canQueryLocalFog = false;
             }
         }
+        return canQueryLocalFog;
+    }
 
-        LinkedHashMap<UUID, Set<UUID>> sortedMap = fogs.entrySet().stream()
-                .sorted(new FrequencyComparator())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
-        List<UUID> fogsList = new ArrayList<>(sortedMap.keySet());
-
-        Map<UUID, Integer> assignedBlocksCount = new HashMap<>();
-
-        for (UUID fog : fogsList) {
-            assignedBlocksCount.put(fog, 0);
+    public static List<ExecPlan> QP2(HashSet<BlockIdReplicaMetadata> blockIdReplicaMetadataSet, Map<UUID, FogPartition> fogPartitions, UUID fogId) {
+        Map<UUID, UUID> mapping = new HashMap<>();
+        if (canQueryLocalFog(blockIdReplicaMetadataSet, fogPartitions, fogId)) {
+            // resolve using local fog
+            for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
+                mapping.put(Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId()), fogId);
+            }
         }
+        else {
+            // minimize the number of blocks per fog
+            Map<UUID, Set<UUID>> fogs = new HashMap<>();
+            Map<UUID, Set<UUID>> blocks = new HashMap<>();
+            for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
+                UUID blockId = Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId());
+                blocks.put(blockId, new HashSet<>());
+                List<BlockReplica> activeReplicas = blockIdReplicaMetadata.getReplicasList().stream()
+                        .filter(replica -> fogPartitions.get(Utils.getUuidFromMessage(replica.getDeviceId())).isActive())
+                        .collect(Collectors.toList());
+                if (activeReplicas.size() == 0) {
+                    throw new RuntimeException("No Available Replica for " + Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId()));
+                }
+                for (BlockReplica blockReplica : activeReplicas) {
+                    UUID replicaFogId = Utils.getUuidFromMessage(blockReplica.getDeviceId());
+                    blocks.get(blockId).add(replicaFogId);
+                    if (!fogs.containsKey(replicaFogId)) {
+                        fogs.put(replicaFogId, new HashSet<>());
+                    }
+                    fogs.get(replicaFogId).add(blockId);
+                }
+            }
 
-        while(mapping.size() < blockIdReplicaMetadataSet.size()) {
+            LinkedHashMap<UUID, Set<UUID>> sortedMap = fogs.entrySet().stream()
+                    .sorted(new FrequencyComparator())
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+            List<UUID> fogsList = new ArrayList<>(sortedMap.keySet());
+
+            Map<UUID, Integer> assignedBlocksCount = new HashMap<>();
+
             for (UUID fog : fogsList) {
-                UUID selectedBlockId = null;
-                for (UUID blockId : sortedMap.get(fog)) {
-                    if (!mapping.containsKey(blockId)) {
-                        selectedBlockId = blockId;
-                        break;
-                    }
-                }
-                if (selectedBlockId == null) {
-                    continue;
-                }
+                assignedBlocksCount.put(fog, 0);
+            }
 
-                int min = Integer.MAX_VALUE;
-                for (UUID fogCandidate : blocks.get(selectedBlockId)) {
-                    min = Math.min(assignedBlocksCount.get(fogCandidate), min);
-                }
-
-                Set<UUID> fogCandidates = new HashSet<>();
-                for (UUID fogCandidate : blocks.get(selectedBlockId)) {
-                    if (assignedBlocksCount.get(fogCandidate) == min) {
-                        fogCandidates.add(fogCandidate);
-                    }
-                }
-
-                UUID assignedFogId = null;
-                if (fogCandidates.contains(fog)) {
-                    assignedFogId = fog;
-                }
-                else {
-                    for (UUID assignmentCandidate : fogsList) {
-                        if (fogCandidates.contains(assignmentCandidate)) {
-                            assignedFogId = assignmentCandidate;
+            while(mapping.size() < blockIdReplicaMetadataSet.size()) {
+                for (UUID fog : fogsList) {
+                    UUID selectedBlockId = null;
+                    for (UUID blockId : sortedMap.get(fog)) {
+                        if (!mapping.containsKey(blockId)) {
+                            selectedBlockId = blockId;
                             break;
                         }
                     }
-                }
-                mapping.put(selectedBlockId, assignedFogId);
-                assignedBlocksCount.put(assignedFogId, assignedBlocksCount.get(assignedFogId) + 1);
-                for (UUID fogElement : blocks.get(selectedBlockId)) {
-                    sortedMap.get(fogElement).remove(selectedBlockId);
+                    if (selectedBlockId == null) {
+                        continue;
+                    }
+
+                    int min = Integer.MAX_VALUE;
+                    for (UUID fogCandidate : blocks.get(selectedBlockId)) {
+                        min = Math.min(assignedBlocksCount.get(fogCandidate), min);
+                    }
+
+                    Set<UUID> fogCandidates = new HashSet<>();
+                    for (UUID fogCandidate : blocks.get(selectedBlockId)) {
+                        if (assignedBlocksCount.get(fogCandidate) == min) {
+                            fogCandidates.add(fogCandidate);
+                        }
+                    }
+
+                    UUID assignedFogId = null;
+                    if (fogCandidates.contains(fog)) {
+                        assignedFogId = fog;
+                    }
+                    else {
+                        for (UUID assignmentCandidate : fogsList) {
+                            if (fogCandidates.contains(assignmentCandidate)) {
+                                assignedFogId = assignmentCandidate;
+                                break;
+                            }
+                        }
+                    }
+                    mapping.put(selectedBlockId, assignedFogId);
+                    assignedBlocksCount.put(assignedFogId, assignedBlocksCount.get(assignedFogId) + 1);
+                    for (UUID fogElement : blocks.get(selectedBlockId)) {
+                        sortedMap.get(fogElement).remove(selectedBlockId);
+                    }
                 }
             }
         }
@@ -136,33 +163,48 @@ public final class CostModel {
     }
 
     public static List<ExecPlan> QP3(HashSet<BlockIdReplicaMetadata> blockIdReplicaMetadataSet, Map<UUID, FogPartition> fogPartitions, UUID fogId) {
-        Map<UUID, Set<UUID>> fogs = new HashMap<>();
-        Map<UUID, Set<UUID>> blocks = new HashMap<>();
-        for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
-            UUID blockId = Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId());
-            blocks.put(blockId, new HashSet<>());
-            for (BlockReplica blockReplica : blockIdReplicaMetadata.getReplicasList()) {
-                UUID replicaFogId = Utils.getUuidFromMessage(blockReplica.getDeviceId());
-                blocks.get(blockId).add(replicaFogId);
-                if (!fogs.containsKey(replicaFogId)) {
-                    fogs.put(replicaFogId, new HashSet<>());
-                }
-                fogs.get(replicaFogId).add(blockId);
+        Map<UUID, UUID> mapping = new HashMap<>();
+        if (canQueryLocalFog(blockIdReplicaMetadataSet, fogPartitions, fogId)) {
+            // resolve using local fog
+            for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
+                mapping.put(Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId()), fogId);
             }
         }
+        else {
+            Map<UUID, Set<UUID>> fogs = new HashMap<>();
+            Map<UUID, Set<UUID>> blocks = new HashMap<>();
+            for (BlockIdReplicaMetadata blockIdReplicaMetadata : blockIdReplicaMetadataSet) {
+                UUID blockId = Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId());
+                blocks.put(blockId, new HashSet<>());
+                List<BlockReplica> activeReplicas = blockIdReplicaMetadata.getReplicasList().stream()
+                        .filter(replica -> fogPartitions.get(Utils.getUuidFromMessage(replica.getDeviceId())).isActive())
+                        .collect(Collectors.toList());
+                if (activeReplicas.size() == 0) {
+                    throw new RuntimeException("No Available Replica for " + Utils.getUuidFromMessage(blockIdReplicaMetadata.getBlockId()));
+                }
+                for (BlockReplica blockReplica : activeReplicas) {
+                    UUID replicaFogId = Utils.getUuidFromMessage(blockReplica.getDeviceId());
+                    blocks.get(blockId).add(replicaFogId);
+                    if (!fogs.containsKey(replicaFogId)) {
+                        fogs.put(replicaFogId, new HashSet<>());
+                    }
+                    fogs.get(replicaFogId).add(blockId);
+                }
+            }
 
-        LinkedHashMap<UUID, Set<UUID>> sortedMap = fogs.entrySet().stream()
-                .sorted(new FrequencyComparatorReverse())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+            LinkedHashMap<UUID, Set<UUID>> sortedMap = fogs.entrySet().stream()
+                    .sorted(new FrequencyComparatorReverse())
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
-        Map<UUID, UUID> mapping = new HashMap<>();
-        List<UUID> fogsList = new ArrayList<>(sortedMap.keySet());
 
-        for (UUID fog : fogsList) {
-            for (UUID blockId : new ArrayList<>(sortedMap.get(fog))) {
-                mapping.put(blockId, fog);
-                for (UUID fogElement : blocks.get(blockId)) {
-                    sortedMap.get(fogElement).remove(blockId);
+            List<UUID> fogsList = new ArrayList<>(sortedMap.keySet());
+
+            for (UUID fog : fogsList) {
+                for (UUID blockId : new ArrayList<>(sortedMap.get(fog))) {
+                    mapping.put(blockId, fog);
+                    for (UUID fogElement : blocks.get(blockId)) {
+                        sortedMap.get(fogElement).remove(blockId);
+                    }
                 }
             }
         }
